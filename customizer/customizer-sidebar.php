7ba5fc0de77fe43e0994e7e3d4199cc5106d762b
Josh <JPollock412@gmail.com>
2013-07-27 03:57:07 -0400

    set selector and property  values for sidebar

--- customizer/customizer-sidebar.php
+++ customizer/customizer-sidebar.php
@@ -64,31 +64,45 @@ function _sf_customize_sidebar() {
 		'default' => '#fff',
 		'label' => __('Sidebar Background Color', 'sf'),
 		'priority' => 5,
+		'selector' => '#secondary',
+		'property' => 'color',
 	);
 	$sidebar[] = array(
 		'slug'=>'widget_title_color', 
 		'default' => '#000',
-		'label' => __('Widget Title Color', 'sf')
+		'label' => __('Widget Title Color', 'sf'),
+		'selector' => 'h5.widget-title',
+		'property' => 'color',
+		
 	);
 	$sidebar[] = array(
-	'slug'=>'sidebar_text_color', 
-	'default' => '#000',
-	'label' => __('Widget Text Color', 'sf')
+		'slug'=>'sidebar_text_color', 
+		'default' => '#000',
+		'label' => __('Widget Text Color', 'sf'),
+		'selector' => '#secondary',
+		'property' => 'color',
 	);
 	$sidebar[] = array(
 		'slug'=>'sidebar_link_color', 
 		'default' => '#1e73be',
-		'label' => __('Widget Link Color', 'sf')
+		'label' => __('Widget Link Color', 'sf'),
+		'selector' => '#secondary a',
+		'property' => 'color',
+		
 	);
 	$sidebar[] = array(
 		'slug'=>'sidebar_linkHvr_color', 
 		'default' => '#fff',
-		'label' => __('Widget Link Hover Color', 'sf')
+		'label' => __('Widget Link Hover Color', 'sf'),
+		'selector' => '#secondary a:hover',
+		'property' => 'color',
 	);
 	$sidebar[] = array(
 		'slug'=>'sidebar_linkVstd_color', 
 		'default' => '#800080',
-		'label' => __('Widget Visited Link Color', 'sf')
+		'label' => __('Widget Visited Link Color', 'sf'),
+		'selector' => '#secondary a:visited',
+		'property' => 'color',
 	);
 	
 	$colors = $sidebar;
